7ecc6dddacb794ae005d57c6bebc1049763e8b0a
Shanmukha Katuri <shanmukha.k@gmail.com>
2023-05-08 15:16:50 +0530

    Update build.mac.sh

--- build.mac.sh
+++ build.mac.sh
@@ -4,7 +4,7 @@ mvn clean compile
 echo [2/$TS] Building native image
 sdk install java 22.3.r19-grl
 sdk use java 22.3.r19-grl
-native-image -cp target/classes --static -o hw com.rabbit.examples.HelloWorld
+native-image -cp target/classes -o hw com.rabbit.examples.HelloWorld
 echo [3/$TS] Preparing the UPX for compression
 brew install upx-ucl -y
 echo [4/$TS] Compressing the binary
