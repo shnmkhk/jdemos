3a97ea9f8c15f8bf614a29a52ecbb24b0ee4d3c8
Shanmukha Katuri <shanmukha.k@gmail.com>
2023-05-08 15:15:03 +0530

    Update build.mac.sh

--- build.mac.sh
+++ build.mac.sh
@@ -2,9 +2,11 @@ TS=5
 echo [1/$TS] Compiling the source files
 mvn clean compile
 echo [2/$TS] Building native image
+sdk install java 22.3.r19-grl
+sdk use java 22.3.r19-grl
 native-image -cp target/classes --static -o hw com.rabbit.examples.HelloWorld
 echo [3/$TS] Preparing the UPX for compression
-sudo brew install upx-ucl -y
+brew install upx-ucl -y
 echo [4/$TS] Compressing the binary
 /usr/bin/upx --lzma --best hw -o hw.upx
 echo [5/$TS] Building the docker image
